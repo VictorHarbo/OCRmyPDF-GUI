6b8686f9ffa388e904306cea6005da27431ddb9d
Victor Harbo Johnston <vhol@kb.dk>
2022-10-24 20:21:21 +0200

    Add new files

---
+++ custommultipleframes.py
@@ -0,0 +1,210 @@
+import tkinter as tk
+import tkinter.messagebox
+import customtkinter
+
+LARGE_FONT= ("Verdana", 12)
+
+
+class MultiPage(customtkinter.CTk):
+    WIDTH = 780
+    HEIGHT = 520
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+
+        self.title("OCRmyPDF")
+        self.geometry(f"{MultiPage.WIDTH}x{MultiPage.HEIGHT}")
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)  # call .on_closing() when app gets closed
+
+        # ============ create two frames ============
+
+        # configure grid layout (2x1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.frame_left = customtkinter.CTkFrame(master=self,
+                                                 width=180,
+                                                 corner_radius=0)
+        self.frame_left.grid(row=0, column=0, sticky="nswe")
+
+        self.frame_right = customtkinter.CTkFrame(master=self)
+        self.frame_right.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
+
+        for F in (OcrPage, PageOne, PageTwo):
+
+            frame = F(frame_right, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(OcrPage)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+        
+class OcrPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        # configure grid layout (3x7)
+        self.frame_right.rowconfigure((0, 1, 2, 3), weight=1)
+        self.frame_right.rowconfigure(7, weight=10)
+        self.frame_right.columnconfigure((0, 1), weight=1)
+        self.frame_right.columnconfigure(2, weight=0)
+
+        self.frame_info = customtkinter.CTkFrame(master=self.frame_right)
+        self.frame_info.grid(row=0, column=0, columnspan=2, rowspan=4, pady=20, padx=20, sticky="nsew")
+        
+        # ============ frame_info ============
+
+        # configure grid layout (1x1)
+        self.frame_info.rowconfigure(0, weight=1)
+        self.frame_info.columnconfigure(0, weight=1)
+
+        self.label_info_1 = customtkinter.CTkLabel(master=self.frame_info,
+                                                   text="CTkLabel: Lorem ipsum dolor sit,\n" +
+                                                        "amet consetetur sadipscing elitr,\n" +
+                                                        "sed diam nonumy eirmod tempor" ,
+                                                   height=100,
+                                                   corner_radius=6,  # <- custom corner radius
+                                                   fg_color=("white", "gray38"),  # <- custom tuple-color
+                                                   justify=tkinter.LEFT)
+        self.label_info_1.grid(column=0, row=0, sticky="nwe", padx=15, pady=15)
+
+        self.progressbar = customtkinter.CTkProgressBar(master=self.frame_info)
+        self.progressbar.grid(row=1, column=0, sticky="ew", padx=15, pady=15)
+
+        # ============ frame_right ============
+
+        self.radio_var = tk.IntVar(value=0)
+
+        self.label_radio_group = customtkinter.CTkLabel(master=self.frame_right,
+                                                        text="CTkRadioButton Group:")
+        self.label_radio_group.grid(row=0, column=2, columnspan=1, pady=20, padx=10, sticky="")
+
+        self.radio_button_1 = customtkinter.CTkRadioButton(master=self.frame_right,
+                                                           variable=self.radio_var,
+                                                           value=0)
+        self.radio_button_1.grid(row=1, column=2, pady=10, padx=20, sticky="n")
+
+        self.radio_button_2 = customtkinter.CTkRadioButton(master=self.frame_right,
+                                                           variable=self.radio_var,
+                                                           value=1)
+        self.radio_button_2.grid(row=2, column=2, pady=10, padx=20, sticky="n")
+
+        self.radio_button_3 = customtkinter.CTkRadioButton(master=self.frame_right,
+                                                           variable=self.radio_var,
+                                                           value=2)
+        self.radio_button_3.grid(row=3, column=2, pady=10, padx=20, sticky="n")
+
+        self.slider_1 = customtkinter.CTkSlider(master=self.frame_right,
+                                                from_=0,
+                                                to=1,
+                                                number_of_steps=3,
+                                                command=self.progressbar.set)
+        self.slider_1.grid(row=4, column=0, columnspan=2, pady=10, padx=20, sticky="we")
+
+        self.slider_2 = customtkinter.CTkSlider(master=self.frame_right,
+                                                command=self.progressbar.set)
+        self.slider_2.grid(row=5, column=0, columnspan=2, pady=10, padx=20, sticky="we")
+
+        self.switch_1 = customtkinter.CTkSwitch(master=self.frame_right,
+                                                text="CTkSwitch")
+        self.switch_1.grid(row=4, column=2, columnspan=1, pady=10, padx=20, sticky="we")
+
+        self.switch_2 = customtkinter.CTkSwitch(master=self.frame_right,
+                                                text="CTkSwitch")
+        self.switch_2.grid(row=5, column=2, columnspan=1, pady=10, padx=20, sticky="we")
+
+        self.combobox_1 = customtkinter.CTkComboBox(master=self.frame_right,
+                                                    values=["Value 1", "Value 2"])
+        self.combobox_1.grid(row=6, column=2, columnspan=1, pady=10, padx=20, sticky="we")
+
+        self.check_box_1 = customtkinter.CTkCheckBox(master=self.frame_right,
+                                                     text="CTkCheckBox")
+        self.check_box_1.grid(row=6, column=0, pady=10, padx=20, sticky="w")
+
+        self.check_box_2 = customtkinter.CTkCheckBox(master=self.frame_right,
+                                                     text="CTkCheckBox")
+        self.check_box_2.grid(row=6, column=1, pady=10, padx=20, sticky="w")
+
+        self.entry = customtkinter.CTkEntry(master=self.frame_right,
+                                            width=120,
+                                            placeholder_text="CTkEntry")
+        self.entry.grid(row=8, column=0, columnspan=2, pady=20, padx=20, sticky="we")
+
+        self.button_5 = customtkinter.CTkButton(master=self.frame_right,
+                                                text="CTkButton",
+                                                border_width=2,  # <- custom border_width
+                                                fg_color=None,  # <- no fg_color
+                                                command=self.button_event)
+        self.button_5.grid(row=8, column=2, columnspan=1, pady=20, padx=20, sticky="we")
+
+        # set default values
+        self.optionmenu_1.set("Dark")
+        self.combobox_1.set("CTkCombobox")
+        self.radio_button_1.select()
+        self.slider_1.set(0.2)
+        self.slider_2.set(0.7)
+        self.progressbar.set(0.5)
+        self.switch_2.select()
+        self.radio_button_3.configure(state=tk.DISABLED)
+        self.check_box_1.configure(state=tk.DISABLED, text="CheckBox disabled")
+        self.check_box_2.select()
+
+    def button_event(self):
+        print("Button pressed")
+
+    def change_appearance_mode(self, new_appearance_mode):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button = tk.Button(self, text="Visit Page 1",
+                            command=lambda: controller.show_frame(PageOne))
+        button.pack()
+
+        button2 = tk.Button(self, text="Visit Page 2",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
+
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page One!!!", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = tk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(OcrPage))
+        button1.pack()
+
+        button2 = tk.Button(self, text="Page Two",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page Two!!!", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = tk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(OcrPage))
+        button1.pack()
+
+        button2 = tk.Button(self, text="Page One",
+                            command=lambda: controller.show_frame(PageOne))
+        button2.pack()
+        
+
+
+app = MultiPage()
+app.mainloop()
