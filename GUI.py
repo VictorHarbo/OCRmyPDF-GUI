b0a4636955a7f9656388e72afb4285f23af3026b
Victor Harbo Johnston <vhol@kb.dk>
2022-10-25 22:04:48 +0200

    Add buttons to startPage

--- GUI.py
+++ GUI.py
@@ -1,8 +1,10 @@
 from tkinter import *
-from tkinter import font
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
 import tkinter.messagebox
 from tkinter.ttk import Style
 import customtkinter as ctk
+import ocrmypdf
 
 ctk.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 ctk.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -10,8 +12,13 @@ ctk.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark
 # Create an instance of tkinter frame or window
 win = ctk.CTk()
 
+# === GLOBAL VARIABLES ===
+inputVariable = StringVar()
+outputVariable = StringVar()
+
 # Set the size of the window
 win.geometry("700x350")
+win.title("OCRmyPDF")
 
 # Create leftframe for buttons
 buttonFrame = ctk.CTkFrame(win)
@@ -31,19 +38,67 @@ def change_to_advancedSettings():
    advancedSettings.pack(fill='both', expand=TRUE, side=RIGHT)
    startPage.pack_forget()
 
-#TODO: Figure out if styles are actually of use to me
-# === Styles ===
-headingStyle = Style()
-headingStyle.configure(style='headingStyle', font=('Arial', 16, 'bold'))
+def select_file():
+   filetypes = (
+     ('PFD files', '*.pdf'),
+     ('All files', '*.*')
+   )
+
+   filename1 = fd.askopenfilename(
+      title='Open a file',
+      initialdir='~',
+      filetypes=filetypes
+   )
+
+   inputVariable.set(filename1)
+
+def save_file():
+   filetypes = (
+      ('PDF files', '*.pdf'),
+      ('All files', '*.*')
+   )
+
+   filename2 = fd.asksaveasfilename(
+      title='Open a file',
+      initialdir='~',
+      filetypes=filetypes
+   )
+   #TODO: Figure out why it says it has two inputs
+   outputVariable.get(filename2)
 
 # === Right frames ===
 # Add content to right frames
+# = Main frame =
 label1 = ctk.CTkLabel(startPage, text="Basic usage here")
-label1.pack(pady=20)
-#TODO Add input file box
-#TODO Add language drop down menu - should these be checkboxes?
-#TODO Add output file box
+label1.pack(pady=5)
 
+inputButton = ctk.CTkButton(
+   startPage,
+   text='Input file',
+   command=select_file
+)
+
+outputButton = ctk.CTkButton(
+   startPage,
+   text='Output file',
+   command=save_file
+)
+
+inputButton.pack(expand=True, side=LEFT)
+outputButton.pack(expand=True, side=RIGHT)
+
+inputEntry = ctk.CTkEntry(startPage, textvariable = inputVariable)
+inputEntry.pack(pady=5, side=LEFT)
+
+outputEntry = ctk.CTkEntry(startPage, textvariable= outputVariable)
+outputEntry.pack(pady=5, side=RIGHT)
+
+#TODO Add language drop down menu - should these be checkboxes?
+#TODO Add RUN button
+#TODO Send input and output to command line OCRmyPDF
+
+
+# = Advanced settings =
 label2 = ctk.CTkLabel(advancedSettings, text="Advanced settings here")
 label2.pack(pady=20)
 #TODO: Advanced options - what should be included?
@@ -60,7 +115,6 @@ btn2.pack(side=TOP, pady=5)
 
 startPage.pack(fill='both', expand=TRUE, side=RIGHT)
 win.mainloop()
-
 
 
 
